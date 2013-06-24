d1622a027944882d639ddcaa0650c85523e1d368
Kyle Decot <kyle.decot@me.com>
2013-06-24 15:04:45 -0400

    Adds spec for when things go wrong w/ `get`

--- spec/kmdata_spec.rb
+++ spec/kmdata_spec.rb
@@ -5,15 +5,31 @@ require "pry"
 describe KMData do
   describe '.get' do
     let(:json) { File.read(File.join('spec', 'fixtures', 'terms.json')) }
-    it 'returns an array of terms' do
-      response = double()
-      response.stub!(:code) { "200" }
-      response.stub!(:body) { json }
 
-      KMData.stub!(:path_with_params).with("/api/terms.json", {})
-      KMData.stub!(:request).with(anything).and_return(response)
-      KMData.get('terms').should == KMData.send(:process, JSON.parse(json))
+    context 'successful' do
+      it 'returns an array of terms' do
+        response = double()
+        response.stub!(:code) { "200" }
+        response.stub!(:body) { json }
+
+        KMData.stub!(:path_with_params).with("/api/terms.json", {})
+        KMData.stub!(:request).with(anything).and_return(response)
+        KMData.get('terms').should == KMData.send(:process, JSON.parse(json))
+      end
     end
+
+    context 'unsuccessful' do
+      it 'returns nil' do
+        response = double()
+        response.stub!(:code) { "500" }
+        response.stub!(:body) { nil }
+
+        KMData.stub!(:path_with_params).with("/api/terms.json", {})
+        KMData.stub!(:request).with(anything).and_return(response)
+        KMData.get('terms').should be_nil
+      end
+    end
+
   end
 
   describe '.process' do
