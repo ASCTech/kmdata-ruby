99289193ea5bd119d3675a757ff7ecdc6dcab900
Kyle Decot <kyle.decot@me.com>
2013-06-24 14:47:58 -0400

    refactoring

--- spec/kmdata_spec.rb
+++ spec/kmdata_spec.rb
@@ -2,24 +2,9 @@ require "spec_helper"
 require "kmdata"
 
 describe KMData do
-  describe '.http' do
-    subject { KMData.send(:http) }
-    it { should respond_to(:request) }
-  end
-
-  describe '.request' do
-    subject { KMData.send(:request, '/api/terms.json') }
-    before :each do
-      response = Net::HTTPResponse.new('', '', '')
-      Net::HTTP.any_instance.stub(:request).with(anything).and_return(response)
-    end
-    it { should respond_to(:body) }
-    it { should respond_to(:code) }
-  end
-
   describe '.get' do
+    let(:json) { File.read(File.join('spec', 'fixtures', 'terms.json')) }
     it 'returns an array of terms' do
-      json = File.read(File.join('spec', 'fixtures', 'terms.json'))
       response = double()
       response.stub!(:code) { "200" }
       response.stub!(:body) { json }
@@ -28,5 +13,20 @@ describe KMData do
       KMData.stub!(:request).with(anything).and_return(response)
       KMData.get('terms').should == KMData.send(:process, JSON.parse(json))
     end
+  end
+
+  describe '.http' do
+    subject { KMData.instance_eval { http } }
+    it { should respond_to(:request) }
+  end
+
+  describe '.request' do
+    subject { KMData.instance_eval { request('/api/terms.json') } }
+    before :each do
+      response = Net::HTTPResponse.new('', '', '')
+      Net::HTTP.any_instance.stub(:request).with(anything).and_return(response)
+    end
+    it { should respond_to(:body) }
+    it { should respond_to(:code) }
   end
 end
