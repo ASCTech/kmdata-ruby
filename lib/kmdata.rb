9f958edc38807f275fe79f2c9ada3fc05dfa7b69
Kyle Decot <kyle.decot@me.com>
2013-06-24 12:34:12 -0400

    No need for that class << self thing

--- lib/kmdata.rb
+++ lib/kmdata.rb
@@ -2,36 +2,35 @@
 
 module KMData
   ENDPOINT = "kmdata.osu.edu"
-  class << self
-    def get(path, params = {})
-      path = path_with_params("/api/#{path}.json", params)
-      response = http.request(Net::HTTP::Get.new(path))
-      process(JSON.parse(response.body)) if response.code == "200"
-    rescue Exception => exception
-    end
 
-    private
+  def self.get(path, params = {})
+    path = path_with_params("/api/#{path}.json", params)
+    response = http.request(Net::HTTP::Get.new(path))
+    process(JSON.parse(response.body)) if response.code == "200"
+  rescue Exception => exception
+  end
 
-    def http
-      @http ||= begin
-        http = Net::HTTP.new(ENDPOINT, 443)
-        http.use_ssl = true
-        http
-      end
-    end
+  private
 
-    def process(json)
-      if json.is_a? Array
-        json.map { |element| process(element) }
-      elsif json.is_a? Hash
-        OpenStruct.new(Hash[json.map { |key, value| [key, process(value)] }])
-      else
-        json
-      end
+  def self.http
+    @http ||= begin
+      http = Net::HTTP.new(ENDPOINT, 443)
+      http.use_ssl = true
+      http
     end
+  end
 
-    def path_with_params(path, params = {})
-      [path, URI.encode_www_form(params)].join("?")
+  def self.process(json)
+    if json.is_a? Array
+      json.map { |element| process(element) }
+    elsif json.is_a? Hash
+      OpenStruct.new(Hash[json.map { |key, value| [key, process(value)] }])
+    else
+      json
     end
+  end
+
+  def self.path_with_params(path, params = {})
+    [path, URI.encode_www_form(params)].join("?")
   end
 end
